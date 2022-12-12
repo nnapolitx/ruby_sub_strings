b0817bf3bb083010dd011bc95772695577691864
nnapolitx <nnapolitano88@gmail.com>
2022-12-12 10:26:58 -0300

    begin writing method

--- ruby_sub_strings.rb
+++ ruby_sub_strings.rb
@@ -1,2 +1,7 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+def substrings(word, dictionary)
+    p word
+end
+
+substrings('below', dictionary)
